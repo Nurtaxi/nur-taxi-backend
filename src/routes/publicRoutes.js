1f23429ac17f6d510924d495b86b80e42a3f8a39
Nurtaxi <xayolparasramantik@gmail.com>
2026-06-14 17:45:22 +0500

    Update publicRoutes.js

--- src/routes/publicRoutes.js
+++ src/routes/publicRoutes.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const { listActiveRegions, runSeed } = require('../controllers/publicController');
+const { listActiveRegions } = require('../controllers/publicController');
 
 const router = express.Router();
 
 router.get('/regions', listActiveRegions);
-router.get('/setup-seed', runSeed);
 
 module.exports = router;
